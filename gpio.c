00fa7824fec16162f599e556c3b40f86df12c6c4
Teresa Hurtado <matehurgal@gmail.com>
2025-04-01 21:15:59 -0600

    Update gpio.c

--- gpio.c
+++ gpio.c
@@ -1,9 +1,7 @@
-#include <stm32f10x.h>
-
 void init_gpio(void){
 	//init portA
 	RCC->APB2ENR |= 1 << 2; //enable clk
-
+	
 	/***********pwm***************/
 	//PA1 mode 2MHz.
 	GPIOA->CRL |= (1 << 5);
@@ -12,14 +10,12 @@ void init_gpio(void){
 	GPIOA->CRL |= (1 << 7);
 	GPIOA->CRL &= ~(1 << 6);
 
-	GPIOA->CRL |= (1 << 27);
-	GPIOA->CRL &= ~(1 << 26);
-	//PA0 alternate function pushpull
-	GPIOA->CRL |= (1 << 25);
-	GPIOA->CRL &= ~(1 << 24);
-	
 	//alternate function remap
 	AFIO->MAPR &= ~((1 << 9)|(1 << 8));
+	GPIOA ->CRL |= (1 << 29);
+	GPIOA ->CRL &= ~(1 << 28);
+	GPIOA ->CRL |= (1 << 31);
+	GPIOA ->CRL &= ~ (1 << 30);
 	
-
 }
+
